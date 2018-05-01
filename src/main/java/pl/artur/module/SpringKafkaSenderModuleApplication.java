b1d98f528e17ac880708d756b5118de7db0adfd4
Artur Laptop <sensibles93@gmail.com>
2018-05-01 15:18:04 +0200

    Initial commit

---
+++ src/main/java/pl/artur/module/SpringKafkaSenderModuleApplication.java
@@ -0,0 +1,12 @@
+package pl.artur.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaSenderModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaSenderModuleApplication.class, args);
+	}
+}
